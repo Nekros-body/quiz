b40036634b2ce954cbd5618eaaa4c64930c6b987
Nekros-body <522797@vistacollege.nl>
2024-10-29 12:01:25 +0100

    gjgf

--- script.js
+++ script.js
@@ -25,3 +25,8 @@ function showSlides(n) {
     }
     slides[slideIndex].style.display = "block"; // Display the current slide
 }
+
+// Automatically switch to the next slide every 3 seconds
+setInterval(() => {
+    plusSlides(1);
+}, 3000); // Change 3000 to adjust the interval in milliseconds
